6088dbfbd8e355083e984eeb12651fb377820f4e
Piotr Krzesaj <Piotr.Krzesaj@dazn.com>
2019-09-16 12:45:34 +0200

    added event cell

---
+++ Dazn Code Challenge/EventListCells/EventCell.swift
@@ -0,0 +1,18 @@
+//
+//  EventCell.swift
+//  Dazn Code Challenge
+//
+//  Created by Piotr Krzesaj on 16/09/2019.
+//  Copyright © 2019 dazn. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView?
+    @IBOutlet weak var titleLabel: UILabel?
+    @IBOutlet weak var hourLabel: UILabel?
+    
+    
+}
